31c6a6c6b5c8f1c4576cd421fa80ec1217c60678
Brian <bcoopmn@gmail.com>
2018-03-15 21:19:29 -0500

    Changed fade-in animation to only apply to index nav, rather than all pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   $('.header').load("header.html", function() {
-    $(".header nav").delay(900).animate({"opacity": "1"}, 700);
+    $(".header #fadein").delay(900).animate({"opacity": "1"}, 700);
     $("footer").delay(900).animate({"opacity": "1"}, 700);
   });
+  $('.header-static').load("header-static.html");
   $("#construction").delay(300).animate({"opacity": "1"}, 700);
   $("#background").delay(200).animate({"opacity": "1"}, 700);
 });
