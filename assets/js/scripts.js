ca04478ee4cd1644a7320f0b7d2ec7f5b0156561
Brian <bcoopmn@gmail.com>
2018-03-17 23:53:06 -0500

    Updated file structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $('.header').load("header.html", function() {
-    $(".header #fadein").delay(900).animate({"opacity": "1"}, 700);
+  $('.core').load("core.html", function() {
+    $(".core #fadein").delay(900).animate({"opacity": "1"}, 700);
     $("footer").delay(900).animate({"opacity": "1"}, 700);
   });
-  $('.header-static').load("header-static.html");
+  $('.core-static').load("core-static.html");
   $("#construction").delay(300).animate({"opacity": "1"}, 700);
   $("#background").delay(200).animate({"opacity": "1"}, 700);
 });
