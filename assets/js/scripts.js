a365ee8f913473ea836ad04c39efa339071a7938
Brian Cooper <coope824@umn.edu>
2019-02-14 00:50:58 -0600

    Added fade effects to about page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,5 +5,7 @@ $(document).ready(function(){
   });
   $('.core-static').load("core-static.html");
   $("#construction").delay(300).animate({"opacity": "1"}, 700);
+  $("#brian").delay(200).animate({"opacity": "1"}, 700);
+  $("p").delay(200).animate({"opacity": "1"}, 700);
   $("#background").delay(200).animate({"opacity": "1"}, 700);
 });
