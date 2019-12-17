65980ec7180f82123ebf77e6d714ec90d17c2824
brian <brian@brian-cooper.com>
2019-12-17 02:21:26 -0600

    overhaul of about page content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $('.core-static').load("core-static.html");
   $("#construction").delay(300).animate({"opacity": "1"}, 700);
   $("#brian").delay(200).animate({"opacity": "1"}, 700);
-  $("p").delay(200).animate({"opacity": "1"}, 700);
+  $("#about").delay(200).animate({"opacity": "1"}, 700);
   $(".form-container").delay(200).animate({"opacity": "1"}, 700);
   $("#background").delay(200).animate({"opacity": "1"}, 700);
 });
