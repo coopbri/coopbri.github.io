b8a94cd8d1a35191a49c575b9bbf2d72377a3208
brian <brian@brian-cooper.com>
2020-02-13 13:24:27 -0600

    added responsive nav styles, onload scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.core').load("core.html", function() {
     $(".core #fadein").delay(900).animate({"opacity": "1"}, 700);
     $("footer").delay(900).animate({"opacity": "1"}, 700);
@@ -9,4 +9,21 @@ $(document).ready(function(){
   $("#about").delay(200).animate({"opacity": "1"}, 700);
   $(".form-container").delay(200).animate({"opacity": "1"}, 700);
   $("#background").delay(200).animate({"opacity": "1"}, 700);
+
+  // show menu on button click
+  $(document).delegate('.menu', 'click', function(event) {
+		$(this).addClass('open');
+		event.stopPropagation();
+	});
+
+  // hide menu when clicking on body (outside of menu)
+	$(document).delegate('body', 'click', function(event)  {
+		$('.menu').removeClass('open');
+	});
+
+  // hide menu when click on `x`
+	$(document).delegate('.cls', 'click', function(event) {
+		$('.menu').removeClass('open');
+		event.stopPropagation();
+	});
 });
